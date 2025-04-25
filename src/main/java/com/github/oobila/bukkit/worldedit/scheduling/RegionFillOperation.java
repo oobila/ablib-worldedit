fc6e8aa56740006825f07ad34f124eda523278a5
BuildTools <unconfigured@null.spigotmc.org>
2025-04-25 03:12:50 +0100

    updating editSession to turn off neighbors

--- src/main/java/com/github/oobila/bukkit/worldedit/scheduling/RegionFillOperation.java
+++ src/main/java/com/github/oobila/bukkit/worldedit/scheduling/RegionFillOperation.java
@@ -1,14 +1,21 @@
 package com.github.oobila.bukkit.worldedit.scheduling;
 
 import com.github.oobila.bukkit.common.scheduling.AsyncJob;
+import com.github.oobila.bukkit.common.scheduling.Job;
+import com.github.oobila.bukkit.worldedit.util.WorldEditLibException;
+import com.sk89q.worldedit.EditSession;
+import com.sk89q.worldedit.WorldEdit;
 import com.sk89q.worldedit.WorldEditException;
 import com.sk89q.worldedit.bukkit.BukkitAdapter;
 import com.sk89q.worldedit.math.BlockVector3;
 import com.sk89q.worldedit.regions.Region;
+import com.sk89q.worldedit.util.SideEffect;
+import com.sk89q.worldedit.util.SideEffectSet;
 import org.bukkit.Location;
 import org.bukkit.Material;
 import org.bukkit.World;
 
+import java.util.Map;
 import java.util.Objects;
 
 import static com.github.oobila.bukkit.worldedit.util.WorldEditUtil.editSessionTransaction;
@@ -30,25 +37,29 @@ public class RegionFillOperation extends AsyncJob {
 
     @Override
     public void run() {
-        try {
-            editSessionTransaction(world, editSession -> {
-                WorldEditMaterialPlacer materialPlacer = new WorldEditMaterialPlacer(editSession);
-                int i = 0;
-                for (BlockVector3 blockVector3 : region) {
-                    if(i >= maxBlockBatchCount){
-                        addChildJob(materialPlacer);
-                        materialPlacer = new WorldEditMaterialPlacer(editSession);
-                        i = 0;
-                    }
-                    Location location = BukkitAdapter.adapt(world, blockVector3);
-                    materialPlacer.addBlock(material, location);
-                    i++;
-                }
+        WorldEdit worldEdit = WorldEdit.getInstance();
+        EditSession editSession = worldEdit.newEditSession(region.getWorld());
+        editSession.setSideEffectApplier(new SideEffectSet(Map.of(SideEffect.NEIGHBORS, SideEffect.State.OFF)));
+
+        WorldEditMaterialPlacer materialPlacer = new WorldEditMaterialPlacer(editSession);
+        int i = 0;
+        for (BlockVector3 blockVector3 : region) {
+            if(i >= maxBlockBatchCount){
                 addChildJob(materialPlacer);
-            });
-        } catch (WorldEditException e) {
-            throw new RuntimeException(e);
+                materialPlacer = new WorldEditMaterialPlacer(editSession);
+                i = 0;
+            }
+            Location location = BukkitAdapter.adapt(world, blockVector3);
+            materialPlacer.addBlock(material, location);
+            i++;
         }
+        addChildJob(materialPlacer);
+        addChildJob(new Job() {
+            @Override
+            public void run() {
+                editSession.close();
+            }
+        });
     }
 
     @Override
